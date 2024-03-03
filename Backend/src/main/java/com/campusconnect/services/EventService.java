d1743e9e5a3ab5e5e0c615f1c6478e2b0f30897b
ayush <vasaniayush942@gmail.com>
2024-03-03 19:06:31 +0530

    modified backend

--- Backend/src/main/java/com/campusconnect/services/EventService.java
+++ Backend/src/main/java/com/campusconnect/services/EventService.java
@@ -11,5 +11,11 @@ public interface EventService
     public EventDto createEvent(EventDto eventDto, Long clubId);
 
     public EventDto updateEvent(EventDto eventDto);
-    public EventDto deleteEvent(EventDto eventDto);
+    public void deleteEvent(Long eventId);
+
+    public List<Event> getAllEventsByClub(Long clubId);
+
+    public List<Event> getEventsBWDate(Date date1,Date date2);
+
+    public List<Event> getAllEvent();
 }
